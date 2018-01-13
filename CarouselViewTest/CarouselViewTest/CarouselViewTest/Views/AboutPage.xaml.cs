7f4ebae18c9663d198208cca05700f1e140004e2
Martin Zikmund <martinzikmund@sphereline.com>
2018-01-13 23:04:18 +0100

    CarouselView test

---
+++ CarouselViewTest/CarouselViewTest/CarouselViewTest/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CarouselViewTest.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ CarouselViewTest/CarouselViewTest/CarouselViewTest/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CarouselViewTest.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+            BindingContext = this;
+		}
+
+        public ObservableCollection<string> Items { get; } = new ObservableCollection<string>() { "hello", "world", "how", "are", "you" };
+	}
+}
